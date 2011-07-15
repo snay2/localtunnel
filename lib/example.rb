2309f7a3dd69d52f6c2db39e935e96d6702c72ac
Steve Nay <steven.nay@gmail.com>
2011-07-15 13:09:17 -0600

    Improved comments

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 require 'localtunnel/tunnel'
 
-# The key file can be provided either on the command line or by finding
-# the first *.pub file in the user's .ssh directory
+# The key file can be provided either as a command-line argument 
+# or by finding the first *.pub file in the user's .ssh directory
 key_file_name = ARGV.first ? ARGV.first : Dir[File.expand_path('~/.ssh/*.pub')].first
 key_file = File.open(key_file_name, "rb")
 key = key_file.read
 
-# The URL that should be notified when the tunnel is created. In this case, a
-# webhook handled by a Kynetx ruleset
-app_url = "http://webhooks.kynetxapps.net/h/a163x128.dev/set_tunnel"
+# The webhook that should be notified when the tunnel is created. In this case, a
+# webhook handled by a Kynetx ruleset.
+app_url = "http://webhooks.kynetxapps.net/h/a0x0.dev/set_tunnel"
 
 # Create and open the tunnel
 tunnel = LocalTunnel::Tunnel.new(4567, key, app_url)
